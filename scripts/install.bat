3ac5b49e751e82738c7ebe630457dec3503430c1
Alexander <alexander.ivanov.289@gmail.com>
2021-05-17 22:20:26 +0300

    Added installation script for windows. Updated Node packages. Added package-lock.json

---
+++ scripts/install.bat
@@ -0,0 +1,12 @@
+@echo off
+
+python -m venv venv
+
+call ./venv/Scripts/activate
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+npm install --no-optional
+
+cd scripts
--- scripts/install.sh
+++ scripts/install.sh
@@ -2,18 +2,19 @@
 # Backend installation
 # Create virtual environment for the project
 
-echo "Move installation to Docker file"
+# echo "Move installation to Docker file"
 
-# cd ..
-#python3 -m venv venv
+cd ..
+python3 -m venv venv
 
 # Activate virtual environment
-#source venv/bin/activate
+source venv/bin/activate
 
 # Install required python pachages
-#pip3 install -r requirements.txt
+pip3 install --upgrade pip3
+pip3 install -r requirements.txt
 
 # Frontend installation
-# npm install
+npm install --no-optional
 
-# cd scripts
+cd scripts
